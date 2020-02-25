3a7475ad80c07e90fd55ee0c988c98c1ad50c6d4
Vladislav Razoronov <razoronov@ucu.edu.ua>
2020-02-25 19:48:36 +0200

    update

--- Task3.py
+++ Task3.py
@@ -1,7 +1,44 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+import folium
+from twitter_api import twitter2
+import json
+from geopy import Nominatim
+import time
+import shutil
+
+def geocoding(location):
+    """
+    Geocodes location to coordinates
+    """
+    geocoder = Nominatim(user_agent='Programming project')
+    time.sleep(1)
+    loc = geocoder.geocode(location)
+    return loc.latitude,loc.longitude
+
 site = Flask(__name__)
-@site.route('/')
+
+@site.route('/',methods=['GET'])
 def base():
+    request.form['name']
+    print(username)
     return render_template('base.html')
-if __name__ == '__main__':
+@site.route('/map')
+def drawmap():
+    data = twitter2.get_data(username)
+    location_list = []
+    info = json.loads(data)
+    for el in info['users']:
+        if el['location'] == '':
+            continue
+        location_list.append((el['name'],geocoding(el['location'])))
+    map = folium.Map(location_list[0][1])
+    for el in location_list:
+        folium.Marker(el[1],el[0]).add_to(map)
+    map.save('friendmap.html')
+    shutil.move('friendmap.html','templates/friendmap.html')
+    return render_template('friendmap.html')
+
+if __name__=='__main__':
+    username = []
     site.run(debug=True)
+
